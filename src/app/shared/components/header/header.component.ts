5db608b96fbfc857a862fda835ec10884549ac06
Paul Müller <paulrichter.jena@gmail.com>
2024-01-10 10:20:06 +0100

    setting active header element properly - some smaller adjustments

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -39,4 +39,46 @@ export class HeaderComponent implements AfterViewInit{
       header.style.backgroundColor = HEADER_BACKGROUND_SECONDARY;
     }
   }
+
+  boundaries: number[] = [];
+
+  public setBoundaries(numbers: number[]){
+    this.boundaries = [];
+    for(let i = 0; i < numbers.length; i++){
+      if(this.boundaries.length == 0){
+        this.boundaries.push(numbers[i])
+      }else{
+        this.boundaries.push(this.boundaries[i-1] + numbers[i]);
+      }
+    }
+  }
+
+  activeElementSSwitchOffset = 200;
+  public setActiveElement(currentTop: number){
+    if(Math.abs(currentTop) < this.boundaries[0] - this.activeElementSSwitchOffset ){
+      this.setActiveElementImpl(0)
+      return;
+    }
+    for(let i = this.boundaries.length; i > 0; i--){
+      if(Math.abs(currentTop) >= this.boundaries[i-1] - this.activeElementSSwitchOffset){
+        this.setActiveElementImpl(i);
+        return;
+      }
+    }
+  }
+  currentActiveElementTitle: string = "Home";
+
+  setActiveElementImpl(index: number){
+    /* Set the clicked nav-link element active and remove active from current active element */
+    var navEls = document.getElementsByClassName('nav-link');
+    if(this.currentActiveElementTitle.includes(navEls[index].innerHTML)) return;
+
+    this.currentActiveElementTitle = navEls[index].innerHTML;
+    var navElsArray = Array.from(navEls);
+    for(let i = 0; i < navElsArray.length; i++){
+      if(navElsArray[i].classList.contains('active')) navElsArray[i].classList.remove('active')
+
+      if(i == index) navElsArray[i].classList.add('active')
+    }
+  }
 }
